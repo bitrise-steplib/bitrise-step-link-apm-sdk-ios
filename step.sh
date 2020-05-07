b5e6786a6a6dcf930026bbc50ce2c2147d241b23
Shams Ahmed <shamsahmed@me.com>
2020-05-07 11:57:10 +0100

    Updated gems and console logs

--- step.sh
+++ step.sh
@@ -6,14 +6,21 @@ GEMFILE="--gemfile=${THIS_SCRIPT_DIR}/Gemfile"
 
 # replace preinstalled bundler on build VM
 if [ "$CI" == "true" ]; then
+    echo "Installing/Updating gems from bundler list"
+
     gem install bundler --force
     gem update --system
 
     bundle update --bundler $GEMFILE
+
+    echo "Installed/Updated gems from bundler list"
 fi  
+
+echo "Installing step dependencies"
 
 #install step dependencies
 bundle install $GEMFILE
 
-bundle exec $GEMFILE ruby $THIS_SCRIPT_DIR/step.rb
+echo "Pre-setup complete for Trace SDK script"
 
+bundle exec $GEMFILE ruby $THIS_SCRIPT_DIR/step.rb
