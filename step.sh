65c7885058e029cd7ad47c922e26f5d6d2801320
Shams Ahmed <shamsahmed@me.com>
2020-11-18 19:23:45 +0000

    Better logs for gem install

--- step.sh
+++ step.sh
@@ -6,23 +6,43 @@ echo "Installing step dependencies"
 
 # dependencies
 if ! gem list -i "^down$"; then 
+    echo "Installing gem: down"
+
     gem install "down" --silent --conservative
+else
+    echo "Found gem: down"
 fi;
 
-if ! gem list -i "^json$"; then 
+if ! gem list -i "^json$"; then
+    echo "Installing gem: json" 
+
     gem install "json" --silent --conservative
+else
+    echo "Found gem: json"
 fi;
 
-if ! gem list -i "^plist$"; then  
+if ! gem list -i "^plist$"; then 
+    echo "Installing gem: plist"
+
     gem install "plist" --silent --conservative
+else
+    echo "Found gem: plist"
 fi;
 
-if ! gem list -i "^xcodeproj$"; then 
+if ! gem list -i "^xcodeproj$"; then
+    echo "Installing gem: xcodeproj" 
+
     gem install "xcodeproj" --silent --conservative
+else
+    echo "Found gem: xcodeproj"
 fi;
 
 if ! gem list -i "^rubyzip$"; then 
+    echo "Installing gem: rubyzip"
+
     gem install "rubyzip" --silent --conservative
+else
+    echo "Found gem: rubyzip"
 fi;
 
 echo "Installed step dependencies"
