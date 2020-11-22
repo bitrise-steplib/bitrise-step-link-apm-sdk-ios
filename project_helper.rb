87830e2b1ee400764c775b8b904384830c24196e
Shams Ahmed <shamsahmed@me.com>
2020-11-22 22:22:22 +0000

    Added timeout and retry to CURL

--- project_helper.rb
+++ project_helper.rb
@@ -128,7 +128,7 @@ class ProjectHelper
           echo "Bitrise Trace SDK - starting Upload dSYM's"
           
           # See script header for more information 
-          /usr/bin/xcrun --sdk macosx swift <(curl -Ls https://raw.githubusercontent.com/bitrise-io/trace-cocoa-sdk/main/UploadDSYM/main.swift)
+          /usr/bin/xcrun --sdk macosx swift <(curl -Ls --retry 3 --connect-timeout 20 https://raw.githubusercontent.com/bitrise-io/trace-cocoa-sdk/main/UploadDSYM/main.swift)
 
           echo "Bitrise Trace SDK - finished Upload dSYM's"
         eos
